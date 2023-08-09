af9dfad738f3d03528799b174a3296492b2f3146
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-08-09 09:46:43 +0700

    Initial Commit

---
+++ choco/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  choco
+//
+//  Created by Azzam Ubaidillah on 09/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ choco/chocoApp.swift
@@ -0,0 +1,17 @@
+//
+//  chocoApp.swift
+//  choco
+//
+//  Created by Azzam Ubaidillah on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct chocoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
